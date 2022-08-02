e3d2181a6884a495ef92cae9a36a33335b7a68f6
agarcan <anselmogarciacantu@gmail.com>
2022-08-02 14:37:24 +0200

    remove date from dictionary in app

--- src/weather/app.py
+++ src/weather/app.py
@@ -38,7 +38,7 @@ def handler(event, context):
     submission_id = submission_data_dict["submission_id"]
     username = submission_data_dict["username"]
 
-    weather_data,date_str = fetch_weather_data.weather_indexes(location, date_str)
+    weather_data,date_str = fetch_weather_data.weather_indexes(location)
 
     ddb.save_submissions_db(username, date_str, submission_id)
     ddb.save_location_db(submission_id, location)
